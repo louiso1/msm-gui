5c2d11c251cc844e5cdd37621227a45e56215fdf
louiso1 <114828731+louiso1@users.noreply.github.com>
2024-04-27 02:03:47 +0000

    directors done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,34 @@
 class DirectorsController < ApplicationController
+  
+  def create
+    d = Director.new
+    d.name = params.fetch("query_name")
+    d.dob = params.fetch("query_dob")
+    d.bio = params.fetch("query_bio")
+    d.image = params.fetch("query_image")
+    d.save
+    redirect_to("/directors")
+  end
+
+  def update
+    the_id = params.fetch("the_id")
+    matching_d = Director.where({ :id => the_id })
+    d = matching_d.first
+    d.name = params.fetch("query_name")
+    d.dob = params.fetch("query_dob")
+    d.bio = params.fetch("query_bio")
+    d.image = params.fetch("query_image")
+    d.save
+    redirect_to("/directors/#{the_id}")
+  end
+
+  def destroy
+    director_id = params.fetch("director_id")
+    matching_director = Director.where({ :id => director_id }).first
+    matching_director.destroy
+    redirect_to("/directors")
+  end
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
